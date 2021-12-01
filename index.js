d106a9d52972a42881fa8f011c4d7f286d1f67a6
Olyana Mushe <olyanamushe@MacBook-Pro-2.local>
2021-11-30 20:45:17 -0500

    pushing to heroku

---
+++ index.js
@@ -0,0 +1,31 @@
+const express= require('express'); 
+
+const app= new express(); //app initializing
+
+
+
+//middleware function init
+//body parser 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse HTML form data
+
+// Initialize ejs Middleware
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+//route <url the client will use and how will the 
+//server respond to the url is requested> 
+//http methods: get() post() put() delete()
+
+app.get('/',(req,res)=> {
+    //res.send(`<h1>Hello From my Express Framework...<h1>`);
+    res.render('index'); 
+
+});
+
+
+
+//start server
+const PORT= process.env.PORT||3000;
+app.listen(PORT,()=> 
+{console.log(`Server started on PORT NO. ${PORT}`)}); 
